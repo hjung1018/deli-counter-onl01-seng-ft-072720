64ccc69415d64a59fb81d88732c1cd3c463f65fc
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-08-03 01:48:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,16 +3,10 @@ def line(deli)
   if deli.length == 0
     puts "The line is currently empty."
   else
-     
     current_line = "The line is currently:"
     deli.each.with_index(1) do |person, i|
       current_line << " #{i}. #{person}"
-    end
+    end   
     puts current_line
   end
 end
-
-# def take_a_number(deli)
-#   if deli.empty?
-#     deli << 
-    
